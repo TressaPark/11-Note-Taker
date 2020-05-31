03183698c1991835f01a227acb2fdd139dc9a5eb
Tressa Park <tressapark@Tressas-MacBook-Pro.local>
2020-05-31 11:38:31 -0500

    ready for heroku!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@ const { v4:uuidv4 } = require ("uuid");
 uuidv4();
 module.exports = function(app) {
     app.get("/api/notes", function (req, res) { 
-        fs.readFile("./dbnotes/db.json","utf8", (err,data) => {
+        fs.readFile("./db/db.json","utf8", (err,data) => {
             if (err) throw err
             const allNotes = JSON.parse(data);
             res.json(allNotes);
@@ -18,11 +18,11 @@ module.exports = function(app) {
             title: req.body.title, 
             text: req.body.text
         }
-        fs.readFile("./dbnotes/db.json","utf8", (err,data) => {
+        fs.readFile("./db/db.json","utf8", (err,data) => {
             if (err) throw err
             const allNotes = JSON.parse(data)
             allNotes.push(newNote)
-        fs.writeFile("./dbnotes/db.json",JSON.stringify(allNotes, null, 2), err => {
+        fs.writeFile("./db/db.json",JSON.stringify(allNotes, null, 2), err => {
             if (err) throw err
             res.redirect("/")  
             console.log("Note Created") 
@@ -31,11 +31,11 @@ module.exports = function(app) {
         });
     app.delete("/api/notes/:id", function (req, res) {
         let noteId = req.params.id
-        fs.readFile("./dbnotes/db.json","utf8", (err,data) => {
+        fs.readFile("./db/db.json","utf8", (err,data) => {
             if (err) throw err
             const allNotes = JSON.parse(data)
             const updatedNotes = allNotes.filter(note => note.id!= noteId)
-            fs.writeFile("./dbnotes/db.json",JSON.stringify(updatedNotes, null, 2), err => {
+            fs.writeFile("./db/db.json",JSON.stringify(updatedNotes, null, 2), err => {
                 if (err) throw err
                 res.send(true);
                 console.log("Note Deleted")
